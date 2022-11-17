aa35e0a31395a9a088be01fa07db39448871dc9c
Justin Adrian <jadrian2006@gmail.com>
2022-11-17 10:38:06 -0500

    fixed setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-cd files && xargs -i curl -OJL 'http://54.219.170.108:2250/d/1/{}/download' < ../files.list
+mkdir files && cd files && xargs -i curl -OJL 'http://54.219.170.108:2250/d/1/{}/download' < ../files.list
